0ebfc343496fca10ab3bf754ae7a1764a69a5785
muhammed.kado <muhammed.kado@ustek-rfid.com>
2025-03-20 17:43:07 +0300

    change endpoint name

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/upload-cv', [CVController::class, 'analyze']);
 
 Route::get('/jobsearch', [JobSearchController::class, 'searchJobs']);
-Route::post('/search', [JobSearchController::class, 'search']);
+Route::post('/create-cv', [JobSearchController::class, 'search']);
 Route::post('/enhance', [CVController::class, 'enhance']);
 
 
